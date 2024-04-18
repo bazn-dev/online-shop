9da1d0fb5e817158b2ee4fad1ba4ec2dffb39b91
Aliaksei Bazhyn <aliakseibaz@vmsngroup.com>
2024-04-18 19:39:24 +0300

    feat(master): fix bugs

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: 'http://178.172.201.242:8081/api/v1',
     headers: {
-        // 'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': '*',
     }
 });
 
